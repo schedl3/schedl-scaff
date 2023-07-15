aebe6440abe53fa3b0baec8b475dae17180ee7ac
Tomo <tomo@crowdeo.com>
2023-07-15 12:22:22 -0400

    Initialize refactor/singleSource-truth

---
+++ packages/hardhat/hardhat.config.ts
@@ -0,0 +1,86 @@
+// This adds support for typescript paths mappings
+import "tsconfig-paths/register";
+import * as dotenv from "dotenv";
+dotenv.config();
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy";
+import { kebabToCamelCase } from "./utils";
+import scaffoldConfig from "@root/scaffold.config";
+
+const defaultNetwork =
+  scaffoldConfig.targetNetwork.network === "hardhat"
+    ? "localhost"
+    : kebabToCamelCase(scaffoldConfig.targetNetwork.network);
+
+// If not set, it uses ours Alchemy's default API key.
+// You can get your own at https://dashboard.alchemyapi.io
+const providerApiKey = process.env.ALCHEMY_API_KEY || scaffoldConfig.alchemyApiKey;
+// If not set, it uses the hardhat account 0 private key.
+const deployerPrivateKey = process.env.DEPLOYER_PRIVATE_KEY ?? scaffoldConfig.deployerPrivateKey;
+// If not set, it uses ours Etherscan default API key.
+const etherscanApiKey = process.env.ETHERSCAN_API_KEY || scaffoldConfig.etherscanApiKey;
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  defaultNetwork,
+  namedAccounts: {
+    deployer: {
+      // By default, it will take the first Hardhat account as the deployer
+      default: 0,
+    },
+  },
+  networks: {
+    // View the networks that are pre-configured.
+    // If the network you are looking for is not here you can add new network settings
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${providerApiKey}`,
+        enabled: process.env.MAINNET_FORKING_ENABLED === "true",
+      },
+    },
+    mainnet: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey],
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey],
+    },
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey],
+    },
+    arbitrum: {
+      url: `https://arb-mainnet.g.alchemy.com/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey],
+    },
+    arbitrumGoerli: {
+      url: `https://arb-goerli.g.alchemy.com/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey],
+    },
+    optimism: {
+      url: `https://opt-mainnet.g.alchemy.com/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey],
+    },
+    optimismGoerli: {
+      url: `https://opt-goerli.g.alchemy.com/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey],
+    },
+    polygon: {
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey],
+    },
+    polygonMumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey],
+    },
+  },
+  verify: {
+    etherscan: {
+      apiKey: `${etherscanApiKey}`,
+    },
+  },
+};
+
+export default config;
---
+++ scaffold.config.ts
@@ -0,0 +1,51 @@
+import * as chain from "@wagmi/chains";
+
+type ScaffoldConfig = {
+  targetNetwork: chain.Chain;
+  pollingInterval: number;
+  deployerPrivateKey: string;
+  etherscanApiKey: string;
+  alchemyApiKey: string;
+  burnerWallet: {
+    enabled: boolean;
+    onlyLocal: boolean;
+  };
+  walletAutoConnect: boolean;
+};
+
+const scaffoldConfig = {
+  // The network where your DApp lives in
+  targetNetwork: chain.hardhat,
+
+  // The interval at which your front-end polls the RPC servers for new data
+  // it has no effect on the local network
+  pollingInterval: 30000,
+
+  // This is ours Alchemy's default API key.
+  // You can get your own at https://dashboard.alchemyapi.io
+  alchemyApiKey: "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF",
+
+  // hardhat account[0] private key
+  deployerPrivateKey:
+    "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+
+  // Etherscan API key for verifying contracts
+  etherscanApiKey: "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW",
+
+  // Burner Wallet configuration
+  burnerWallet: {
+    // Set it to false to completely remove burner wallet from all networks
+    enabled: true,
+    // Only show the Burner Wallet when running on hardhat network
+    onlyLocal: true,
+  },
+
+  /**
+   * Auto connect:
+   * 1. If the user was connected into a wallet before, on page reload reconnect automatically
+   * 2. If user is not connected to any wallet:  On reload, connect to burner wallet if burnerWallet.enabled is true && burnerWallet.onlyLocal is false
+   */
+  walletAutoConnect: true,
+} satisfies ScaffoldConfig;
+
+export default scaffoldConfig;
