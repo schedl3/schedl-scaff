68f59ae52325491ae6fa5ec3ac69c48fa59fdd84
tomo <tomo@crowdeo.com>
2023-07-23 21:35:02 +0700

    feat: auth, user modules - jwt/local login

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CatsModule } from './cats/cats.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
     // MongooseModule.forRoot('mongodb://localhost:27017/test'),
     MongooseModule.forRoot('mongodb://localhost:27017/cats'),
     CatsModule,
+    AuthModule,
+    UsersModule,
   ],
+  controllers: [AppController],
 })
 export class AppModule {}
