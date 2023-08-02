fe5d95413bd73801d5b310d24f813150e96d3da3
tomo <tomo@crowdeo.com>
2023-08-02 19:38:58 +0700

    feat: siwe w/ browser wallet eg metamask, get JWT
    Proof of concept: Enable session cookies usage by running both the Next.js server and the backend Nest server on HTTPS. Browser must accept improperly signed certificates for SSL on both servers and bypass Metamask's phishing scam warning caused by altered signed message hostname between front-end and back-end.

--- packages/nextjs/components/Challenge.tsx
+++ packages/nextjs/components/Challenge.tsx
@@ -1,18 +1,65 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useAccount, useSignMessage } from "wagmi";
 
-const BACKEND_URL = "http://127.0.0.1:3000";
+const BACKEND_URL = "https://localhost:3000";
 
 export const Challenge = () => {
   const [challenge, setChallenge] = useState("");
+  const [message, setMessage] = useState("gm wagmi frens");
+  const [signature, setSignature] = useState("");
+  const [jwt, setJwt] = useState("");
+  const [error, setError] = useState("");
+  const account = useAccount();
+  const { /* data, isError, isLoading, isSuccess, */ signMessage } = useSignMessage({
+    message,
+    async onSettled(data, error) {
+      console.log("Settled", { data, error });
+      if (error) {
+        setError(error.message);
+
+        return;
+      } else if (data) {
+        setSignature(data);
+        const response = await axios.post(
+          BACKEND_URL + "/auth/ethloginjwt",
+          {
+            message,
+            signature: data,
+            // signature,
+          },
+          { withCredentials: true },
+        );
+        console.log("Signature posted:", response.data);
+        setJwt(response.data.access_token);
+      }
+    },
+  });
 
   const handleChallengeClick = async () => {
     try {
-      const response = await axios.get(BACKEND_URL + "/challenge");
+      const response = await axios.get(BACKEND_URL + "/challenge", { withCredentials: true });
       const { nonce } = response.data; // TODO verify nonce
+      const URI = "https://localhost:3000"; // XXX not web domain
+      const domain = "localhost:3000"; // XXX not window.location.host
       setChallenge(nonce);
+      const template = (domain: string, nonce: string, address: string, date: string) =>
+        `${domain} wants you to sign in with your Ethereum account:\n` +
+        `${address}\n` +
+        "\n" +
+        "Sign in with Ethereum to the app.\n" +
+        "\n" +
+        `URI: ${URI}\n` +
+        "Version: 1\n" +
+        "Chain ID: 1\n" +
+        `Nonce: ${nonce}\n` +
+        `Issued At: ${date}`;
+      setMessage(template(domain, nonce, account.address!, new Date().toISOString()));
+      setSignature("");
+      setError("");
     } catch (error) {
       console.error("Error fetching nonce:", error);
+      setError("Error fetching nonce. Please try again later.");
     }
   };
 
@@ -22,6 +69,12 @@ export const Challenge = () => {
       <button onClick={handleChallengeClick} className="btn btn-sm btn-primary">
         Challenge
       </button>
+      <button onClick={() => signMessage()} disabled={!challenge} className="btn btn-sm btn-primary">
+        Sign and Post
+      </button>
+      {error && <p>Error: {error}</p>}
+      {signature && <p>Signature: {signature}</p>}
+      {jwt && <p>JWT: {jwt}</p>}
     </div>
   );
 };
